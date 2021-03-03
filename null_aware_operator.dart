490db218de75a710f8049fea5fb706969ee6d1a0
ristafn <golemmite23@gmail.com>
2021-03-03 22:26:02 +0700

    first commit

---
+++ null_aware_operator.dart
@@ -0,0 +1,25 @@
+// (?.), (??), (??=)
+
+class Num {
+  int num = 10;
+}
+
+main() {
+  var n;
+  int number;
+
+  /*
+  if (n != null) {
+    number = n.num;
+  }
+  */
+
+  // set n value to 0
+  number = n?.num ?? 0;
+
+  print(number);
+
+  int number2;
+
+  print(number2 ??= 100);
+}
---
+++ playground.dart
@@ -0,0 +1,24 @@
+class Person {
+  String name;
+  int age;
+
+  Person(this.name, [this.age = 18]);
+
+  // named constructor
+  Person.guest() {
+    name = 'Guest';
+    age = 18;
+  }
+  void showOutput() {
+    print(name);
+    print(age);
+  }
+}
+
+void main() {
+  var person1 = Person('Jhomn', 89);
+  person1.showOutput();
+
+  var person2 = Person.guest();
+  person2.showOutput();
+}
