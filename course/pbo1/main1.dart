6285b5a1811be367edfe09d54d7080376b0ccc2d
ristafn <golemmite23@gmail.com>
2021-03-08 10:56:37 +0700

    add null aware(?) and null checker

--- course/pbo1/main1.dart
+++ course/pbo1/main1.dart
@@ -2,16 +2,16 @@ import 'dart:io';
 
 void main(List<String> arguments) {
   // get input string
-  String input = stdin.readLineSync();
+  String? input = stdin.readLineSync();
   print(input);
 
   // get input string and parse to int
-  String input2 = stdin.readLineSync();
-  int number = int.tryParse(input2);
-  print(number + 10);
+  String? input2 = stdin.readLineSync();
+  int? number = int.tryParse(input2!);
+  print(number! + 10);
 
   // or using this
 
-  int n = int.parse(stdin.readLineSync());
+  int n = int.parse(stdin.readLineSync()!);
   print(n);
 }
--- course/pbo2/main2.dart
+++ course/pbo2/main2.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  int number = int.tryParse(stdin.readLineSync());
+  int? number = int.tryParse(stdin.readLineSync()!);
   String output;
 
-  if (number > 0) {
+  if (number! > 0) {
     output = 'positif';
   } else if (number < 0) {
     output = 'negatif';
